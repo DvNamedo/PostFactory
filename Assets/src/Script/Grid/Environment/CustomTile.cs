7cb682d5991d7e12d2a94f754753cfc9d166709c
ddam5807 <damkungddam@gmail.com>
2026-02-21 19:41:43 +0900

    grid 폴더 추가

---
+++ Assets/src/Script/Grid/Environment/CustomTile.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+[CreateAssetMenu(fileName = "New Data Tile", menuName = "나의타일/데이터타일")]
+public class CustomTile : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
